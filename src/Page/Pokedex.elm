d91316d32cb30c323e03faa751ef70f9dbd9b987
Mathias Frithiofsson <mathias.frithiofsson@carboncloud.com>
2022-01-24 15:18:58 +0100

    Cleanup

--- src/Page/Pokedex.elm
+++ src/Page/Pokedex.elm
@@ -1,54 +1,55 @@
 module Page.Pokedex exposing (..)
-import Html
+
 import Html exposing (Html)
-import Html.Attributes
-import Session exposing (Session)
-import Browser exposing (Document)
 import Page exposing (PageView)
-import Platform exposing (Router)
-import Route exposing (Route)
-import Pokemon.API
 import Pokemon exposing (Pokemon(..))
-import RemoteData exposing (WebData)
 import Pokemon.API
-import Html.Events
+import RemoteData exposing (WebData)
+import Session exposing (Session)
+
 
 type Msg
-    = FirstPage
-    | PreviousPage
-    | NextPage
-    | LastPage
+    = Init
     | GotPokemons (WebData (Pokemon.API.Page Pokemon))
 
 
-type alias Model = { session : Session, pokemons : WebData (Pokemon.API.Page Pokemon) } 
+type alias Model =
+    { session : Session, pokemons : WebData (Pokemon.API.Page Pokemon) }
 
-init : Session -> (Model, Cmd Msg)
-init session = ( { session = session, pokemons = RemoteData.NotAsked }, Pokemon.API.getPokemons GotPokemons)
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = case msg of
-    FirstPage ->
-        (model, Pokemon.API.getPokemons GotPokemons)
-    PreviousPage ->
-        (model, Cmd.none)
-    NextPage ->
-        (model, Pokemon.API.getNextPokemons )
-    LastPage ->
-        (model, Cmd.none)
-    GotPokemons pokemons ->
-        ({model | pokemons = pokemons}, Cmd.none)
+init : Session -> ( Model, Cmd Msg )
+init session =
+    ( { session = session, pokemons = RemoteData.NotAsked }, Pokemon.API.getPokemons GotPokemons )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Init ->
+            ( model, Pokemon.API.getPokemons GotPokemons )
+
+        GotPokemons pokemons ->
+            ( { model | pokemons = pokemons }, Cmd.none )
+
 
 view : Model -> PageView Msg
 view model =
     { title = "Pokedex"
-    , content = Html.div [] case model.pokemons of
-        RemoteData.Success x ->
-            [ viewPokemons x.results
-            , Html.button [ Html.Events.onClick NextPage ] [ Html.text Next] ]
-        _ ->
-            [ Html.text "Loading..." ]
+    , content =
+        Html.div [] <|
+            case model.pokemons of
+                RemoteData.Success x ->
+                    [ viewPokemons x.results ]
+
+                _ ->
+                    [ Html.text "Loading..." ]
     }
 
+
 viewPokemons : List Pokemon -> Html Msg
-viewPokemons = Html.ul [] << List.map (\(Pokemon pokemon) -> Html.li [] [Html.text pokemon.name])
+viewPokemons =
+    let
+        viewPokemon (Pokemon pokemon) =
+            Html.li [] [ Html.text pokemon.name ]
+    in
+    Html.ul [] << List.map viewPokemon
--- src/Pokemon/API.elm
+++ src/Pokemon/API.elm
@@ -1,30 +1,32 @@
 module Pokemon.API exposing (..)
 
 import Http
-import RemoteData exposing (WebData)
-import Url exposing (Url)
-import Url.Builder exposing (QueryParameter)
+import Json.Decode as JD exposing (Decoder)
 import Pokemon exposing (Pokemon)
-import Json.Decode as JD
-import Json.Decode exposing (Decoder)
+import RemoteData exposing (WebData)
+import Url.Builder exposing (QueryParameter)
 
-type alias Page a = { count: Int, next : Maybe String, previous : Maybe String, results : List a }
+
+type alias Page a =
+    { count : Int, next : Maybe String, previous : Maybe String, results : List a }
+
 
 url : List String -> List QueryParameter -> String
-url = Url.Builder.crossOrigin "https://pokeapi.co/api/v2"
+url =
+    Url.Builder.crossOrigin "https://pokeapi.co/api/v2"
 
-getPokemons : (WebData (Page Pokemon)-> msg) -> Cmd msg
-getPokemons toMsg = Http.get {
-        url = url ["pokemon"] [],
-        expect = Http.expectJson (toMsg << RemoteData.fromResult) (pageDecoder Pokemon.decoder)
-    }
+getPokemons : (WebData (Page Pokemon) -> msg) -> Cmd msg
+getPokemons toMsg =
+    Http.get
+        { url = url [ "pokemon-species"] [ Url.Builder.string "limit" "1000"]
+        , expect = Http.expectJson (toMsg << RemoteData.fromResult) (pageDecoder Pokemon.decoder)
+        }
 
-getNextPage : Page a -> Cmd msg
-getNextPage =  
 
 pageDecoder : Decoder a -> Decoder (Page a)
-pageDecoder resultsDecoder= JD.map4 Page 
-    (JD.field "count" JD.int)
-    (JD.field "next" <| JD.maybe JD.string)
-    (JD.field "previous" <| JD.maybe JD.string)
-    (JD.field "results" <| JD.list resultsDecoder)
+pageDecoder resultsDecoder =
+    JD.map4 Page
+        (JD.field "count" JD.int)
+        (JD.field "next" <| JD.maybe JD.string)
+        (JD.field "previous" <| JD.maybe JD.string)
+        (JD.field "results" <| JD.list resultsDecoder)
