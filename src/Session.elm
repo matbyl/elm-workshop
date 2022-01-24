d5dc605c286a209ca86cae475635b9cf956fc3cf
Mathias Frithiofsson <mathias.frithiofsson@carboncloud.com>
2022-01-24 15:30:08 +0100

    Revert to minimal start

--- src/Session.elm
+++
@@ -1,8 +0,0 @@
-module Session exposing (..)
-
-import Browser.Navigation as Navigation
-
-
-type CatApiKey  = CatApiKey String
-
-type alias Session = { navKey : Navigation.Key, catApiKey : CatApiKey }
