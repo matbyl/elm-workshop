03f64bbd414e015f2c571428785c5d30cb7eceef
Mathias Frithiofsson <mathias.frithiofsson@carboncloud.com>
2022-01-24 17:12:47 +0100

    format

--- src/Main.elm
+++ src/Main.elm
@@ -19,25 +19,29 @@ type Msg
     | Decrement
 
 
-type alias Model = Int
+type alias Model =
+    Int
 
 
 init : Model
-init = 0
+init =
+    0
 
 
 update : Msg -> Model -> Model
-update msg model = case msg of
-    Increment ->
-        model + 1
-    Decrement ->
-        model - 1
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
 
+        Decrement ->
+            model - 1
 
 
 view : Model -> Html Msg
-view model = div [] [
-        button [ onClick Decrement ] [ text "-"]
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
         , text <| "Clicked " ++ String.fromInt model
-        , button [ onClick Increment ] [text "+"]
-    ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
