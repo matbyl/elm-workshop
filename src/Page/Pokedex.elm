16dbbce8049a508a0299d3a12212b90fc41c9715
Mathias Frithiofsson <mathias.frithiofsson@carboncloud.com>
2022-01-24 17:09:44 +0100

    format

--- src/Page/Pokedex.elm
+++ src/Page/Pokedex.elm
@@ -1,30 +1,37 @@
 module Page.Pokedex exposing (..)
+
+import Html exposing (Html, div, text)
+import Page exposing (Page)
 import RemoteData exposing (WebData)
 import Session exposing (Session)
-import Html exposing (div)
-import Html exposing (text)
-import Html exposing (Html)
-import Page exposing (Page)
-
-type alias Model = { session : Session, pokemons : WebData (List Pokemon) }
-
-type alias Pokemon = { name : String }
 
 
-init : Session -> (Model, Cmd Msg)
-init session = ({ session = session, pokemons = RemoteData.Loading }, Cmd.none)
+type alias Model =
+    { session : Session, pokemons : WebData (List Pokemon) }
+
+
+type alias Pokemon =
+    { name : String }
+
+
+init : Session -> ( Model, Cmd Msg )
+init session =
+    ( { session = session, pokemons = RemoteData.Loading }, Cmd.none )
+
 
 type Msg
     = GotPokemons
 
-update : Msg -> Model ->  ( Model, Cmd Msg )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotPokemons ->
-             ( model, Cmd.none )
+            ( model, Cmd.none )
+
 
 view : Model -> Page Msg
 view _ =
     { title = "Pokedex"
-    , content = div [] [ text "Pokedex"] }
-    
+    , content = div [] [ text "Pokedex" ]
+    }
