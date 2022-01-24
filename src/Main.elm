27f6f3bfb643638f69fdb198a9eb724edaf07de6
Mathias Frithiofsson <mathias.frithiofsson@carboncloud.com>
2022-01-24 17:09:56 +0100

    format

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,13 @@ module Main exposing (main)
 
 import Browser exposing (Document)
 import Browser.Navigation
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (default)
+import Html exposing (Html, a, button, div, text)
+import Html.Attributes exposing (default, href)
 import Html.Events exposing (onClick)
 import Json.Encode as JE
 import Route exposing (Route(..))
 import Session exposing (Session)
 import Url exposing (Url)
-import Html.Attributes exposing (href)
-import Html exposing (a)
 
 
 main : Program JE.Value Model Msg
@@ -31,12 +29,14 @@ type Msg
     | OnUrlRequest Browser.UrlRequest
 
 
-type HomeMsg = Increment
+type HomeMsg
+    = Increment
     | Decrement
+
 
 type Model
     = Home { session : Session, counter : Int }
-    | MyNewPage { session : Session}
+    | MyNewPage { session : Session }
 
 
 defaultModel : Browser.Navigation.Key -> Model
@@ -48,37 +48,44 @@ init : JE.Value -> Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
 init _ _ navKey =
     ( defaultModel navKey, Cmd.none )
 
+
 toSession : Model -> Session
-toSession model = case model of
-            Home {session } ->
-                session
-            MyNewPage {session} ->
-                session
+toSession model =
+    case model of
+        Home { session } ->
+            session
+
+        MyNewPage { session } ->
+            session
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     let
-        session = toSession model
+        session =
+            toSession model
     in
-    case (model, msg) of
-        (Home homeModel, GotHomeMsg subMsg) ->
+    case ( model, msg ) of
+        ( Home homeModel, GotHomeMsg subMsg ) ->
             case subMsg of
                 Increment ->
                     ( Home { homeModel | counter = homeModel.counter + 1 }, Cmd.none )
 
                 Decrement ->
                     ( Home { homeModel | counter = homeModel.counter - 1 }, Cmd.none )
-        (_, OnUrlChange url) ->
+
+        ( _, OnUrlChange url ) ->
             case Route.fromUrl url of
                 Just Route.Home ->
                     ( defaultModel session.navKey, Cmd.none )
 
                 Just Route.MyNewPage ->
-                    ( MyNewPage { session = session}, Cmd.none )
-                Nothing ->
-                    ( model, Cmd.none)
+                    ( MyNewPage { session = session }, Cmd.none )
 
-        (_, OnUrlRequest urlRequest) ->
+                Nothing ->
+                    ( model, Cmd.none )
+
+        ( _, OnUrlRequest urlRequest ) ->
             case urlRequest of
                 Browser.Internal url ->
                     url.fragment
@@ -88,8 +95,9 @@ update msg model =
                 Browser.External url ->
                     ( model, Browser.Navigation.load url )
 
-        (_, _) ->
-            (model, Cmd.none)
+        ( _, _ ) ->
+            ( model, Cmd.none )
+
 
 view : Model -> Document Msg
 view model =
@@ -101,9 +109,10 @@ view model =
                     [ button [ onClick <| GotHomeMsg Decrement ] [ text "-" ]
                     , text <| "Clicked " ++ String.fromInt homeModel.counter
                     , button [ onClick <| GotHomeMsg Increment ] [ text "+" ]
-                    , a [ href <| Route.toString Route.MyNewPage] [ text "Go to my new page"]
+                    , a [ href <| Route.toString Route.MyNewPage ] [ text "Go to my new page" ]
                     ]
+
             MyNewPage sessino ->
-                div [] [ text "my new page"]
+                div [] [ text "my new page" ]
         ]
     }
--- src/Route.elm
+++ src/Route.elm
@@ -1,24 +1,32 @@
 module Route exposing (..)
-import Url.Parser exposing (Parser)
-import Url.Parser as Parser
+
 import Url exposing (Url)
+import Url.Parser as Parser exposing (Parser)
 
 
-type Route = Home | MyNewPage
+type Route
+    = Home
+    | MyNewPage
+
 
 parser : Parser (Route -> a) a
-parser = Parser.oneOf [
-        Parser.map Home Parser.top
+parser =
+    Parser.oneOf
+        [ Parser.map Home Parser.top
         , Parser.map MyNewPage <| Parser.s "my-new-page"
-    ]
+        ]
+
 
 fromUrl : Url -> Maybe Route
-fromUrl url = Parser.parse parser { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+fromUrl url =
+    Parser.parse parser { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+
 
 toString : Route -> String
-toString route = case route of
-    Home ->
-     "#/"
+toString route =
+    case route of
+        Home ->
+            "#/"
 
-    MyNewPage ->
-     "#/my-new-page"
+        MyNewPage ->
+            "#/my-new-page"
