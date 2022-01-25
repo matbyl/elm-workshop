d64e8da9a2eb3be72a46acb438450f96474d5f0c
Mathias Frithiofsson <mathias.frithiofsson@carboncloud.com>
2022-01-25 12:44:30 +0100

    cleanup

--- src/Pokemon/API.elm
+++ src/Pokemon/API.elm
@@ -8,17 +8,22 @@ import Url.Builder exposing (QueryParameter)
 
 
 type alias Page a =
-    { count : Int, next : Maybe String, previous : Maybe String, results : List a }
+    { count : Int
+    , next : Maybe String
+    , previous : Maybe String
+    , results : List a
+    }
 
 
 url : List String -> List QueryParameter -> String
 url =
     Url.Builder.crossOrigin "https://pokeapi.co/api/v2"
 
+
 getPokemons : (WebData (Page Pokemon) -> msg) -> Cmd msg
 getPokemons toMsg =
     Http.get
-        { url = url [ "pokemon-species"] [ Url.Builder.string "limit" "1000"]
+        { url = url [ "pokemon-species" ] [ Url.Builder.string "limit" "1000" ]
         , expect = Http.expectJson (toMsg << RemoteData.fromResult) (pageDecoder Pokemon.decoder)
         }
 
